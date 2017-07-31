cd2f9bbf4a7535d769f661b78ce38d29452d38bd
qq986945193 <986945193@qq.com>
2017-07-31 10:52:32 +0800

    commit s

--- SSHTools/src/com/qq986945193/davidsshtools/domain/SpringPerson.java
+++ SSHTools/src/com/qq986945193/davidsshtools/domain/SpringPerson.java
@@ -1,5 +1,10 @@
 package com.qq986945193.davidsshtools.domain;
 /**
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ */
+/**
  * 演示构造方法的注入方式
  */
 public class SpringPerson {
