65de5a7cefbb34ac8e4c53893b88bc456f61593e
qq986945193 <986945193@qq.com>
2017-07-31 11:03:44 +0800

    /**  * @Author ：程序员小冰  * @新浪微博 ：http://weibo.com/mcxiaobing  * @GitHub: https://github.com/QQ986945193  */

--- SSHTools/src/com/qq986945193/davidsshtools/dao/impl/SpringCustomerDaoImpl.java
+++ SSHTools/src/com/qq986945193/davidsshtools/dao/impl/SpringCustomerDaoImpl.java
@@ -3,6 +3,11 @@ package com.qq986945193.davidsshtools.dao.impl;
  * spring day1 一个持久层实现类。需要接口这里省略了
  *
  */
+/**
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ */
 public class SpringCustomerDaoImpl {
 
 	/**
